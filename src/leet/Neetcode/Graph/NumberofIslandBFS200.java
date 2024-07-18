59aaf13216c933d5239d048886fd64178390724b
Janani <55023218+jananirangarajr@users.noreply.github.com>
2024-07-18 17:46:55 -0500

    Neetcode heap & backtracking

--- src/leet/Neetcode/Graph/NumberofIslandBFS200.java
+++ src/leet/Neetcode/Graph/NumberofIslandBFS200.java
@@ -2,6 +2,7 @@ package leet.Neetcode.Graph;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class NumberofIslandBFS200 {
@@ -19,11 +20,11 @@ public class NumberofIslandBFS200 {
                 if (grid[i][j] == '1'){
                     count+= 1;
                     grid[i][j] = '0';
-                    Queue<int[]> q = new ArrayDeque<>();
+                    Queue<int[]> q = new LinkedList<>();
                     q.add(new int[]{i,j});
 
-                    while(q != null){
-                        int[] rowCol = q.remove();
+                    while(q.size() > 0){
+                        int[] rowCol = q.poll();
 
                         //check 4 directions and add it to the queue to process 1 set
                         int[][] directons = {{1,0},{-1,0},{0,1},{0,-1}};
@@ -31,7 +32,7 @@ public class NumberofIslandBFS200 {
                             int[] newPair= directons[k];
                             int row = newPair[0]+rowCol[0];
                             int col = newPair[1]+rowCol[1];
-                            if (row > 0 && col > 0 && row < rowLength && col < colLength && grid[row][col] == '1'){
+                            if (row >= 0 && col >= 0 && row < rowLength && col < colLength && grid[row][col] == '1'){
                                 grid[row][col] = '0';
                                 q.add(new int[]{row,col});
                             }
